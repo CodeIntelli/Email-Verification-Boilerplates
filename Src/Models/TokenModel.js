b3ddcc3528d9d72b8799fa517a538f4cadc900a5
rinkal.m <rinkal.m@upsqaure.in>
2022-04-08 13:23:27 +0530

    email through otp verification

--- Src/Models/TokenModel.js
+++ Src/Models/TokenModel.js
@@ -1,16 +1,22 @@
-import mongoose from "mongoose";
+/** @format */
+
+import mongoose from 'mongoose';
 
 const TokenSchema = new mongoose.Schema({
-  userId: {
-    type: mongoose.Schema.ObjectId,
-    required: true,
-    ref: "user",
-    unique: true,
-  },
-  token: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now, expires: 1200 }, // 20 minutes
+	userId: {
+		type: mongoose.Schema.ObjectId,
+		required: true,
+		ref: 'user',
+		unique: true,
+	},
+	token: { type: String, required: true },
+	otp: {
+		type: String,
+		unique: true,
+	},
+	createdAt: { type: Date, default: Date.now, expires: 1200 }, // 20 minutes
 });
 
-let TokenModel = mongoose.model("TokenVerification", TokenSchema);
+let TokenModel = mongoose.model('TokenVerification', TokenSchema);
 
 export default TokenModel;
